92cd815cf445cf8addfa485b67d28b7de5ba6524
Christophe Hurpeau <christophe@hurpeau.com>
2016-04-08 20:45:34 +0200

    react 15

--- browser/lib/App.js
+++ browser/lib/App.js
@@ -24,6 +24,10 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+/**
+ * @function
+ * @param obj
+*/
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
